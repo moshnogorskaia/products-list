c528ee91016fa4058a905f6e3b7ce66001079f9c
Marina Moshnogorskaia <mmoshnogorskaia@gmail.com>
2025-12-15 17:24:25 +0100

    Load paths dynamically

--- pages/[pid].js
+++ pages/[pid].js
@@ -11,13 +11,19 @@ function ProductDetailPage(props) {
   );
 }
 
+export async function getData() {
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  return data;
+}
+
 export async function getStaticProps(context) {
   const { params } = context;
   const productId = params.pid;
 
-  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
-  const jsonData = await fs.readFile(filePath);
-  const data = JSON.parse(jsonData);
+  const data = await getData();
 
   const product = data.products.find((product) => product.id === productId);
 
@@ -25,12 +31,14 @@ export async function getStaticProps(context) {
 }
 
 export async function getStaticPaths() {
+  const data = await getData();
+
+  const pathsWithParams = data.products.map((product) => ({
+    params: { pid: product.id },
+  }));
+
   return {
-    paths: [
-      { params: { pid: 'p1' } },
-      { params: { pid: 'p2' } },
-      { params: { pid: 'p3' } },
-    ],
+    paths: pathsWithParams,
     fallback: 'blocking',
   };
 }
