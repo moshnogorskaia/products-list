f6f2c6b89069600bf1a58251c35d08d39d7a3880
Marina Moshnogorskaia <mmoshnogorskaia@gmail.com>
2025-12-19 09:01:32 +0100

    Combine prefetch with client fetch

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,12 +1,12 @@
 import { useEffect, useState } from 'react';
 import useSWR from 'swr';
 
-function LastSalesPage() {
+function LastSalesPage(props) {
   const { data, error, isLoading } = useSWR(
     'https://nextjs-course-b9d37-default-rtdb.europe-west1.firebasedatabase.app/sales.json',
     (url) => fetch(url).then((res) => res.json())
   );
-  const [sales, setSales] = useState();
+  const [sales, setSales] = useState(props.sales);
 
   useEffect(() => {
     if (data) {
@@ -29,7 +29,7 @@ function LastSalesPage() {
     return <p>Loading...</p>;
   }
 
-  if (!data || !sales) return <p>No sales data found</p>;
+  if (!data && !sales) return <p>Loading...</p>;
 
   return (
     <ul>
@@ -40,6 +40,21 @@ function LastSalesPage() {
       ))}
     </ul>
   );
+}
+
+export async function getStaticProps() {
+  const response = await fetch(
+    'https://nextjs-course-b9d37-default-rtdb.europe-west1.firebasedatabase.app/sales.json'
+  );
+  const data = await response.json();
+  const transformedSales = [];
+  for (const key in data) {
+    transformedSales.push({
+      id: key,
+      ...data[key],
+    });
+  }
+  return { props: { sales: transformedSales } };
 }
 
 export default LastSalesPage;
